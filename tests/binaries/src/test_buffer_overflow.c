d29216f5eae887c54aea5d534e4661215282464e
mabocode <matthias.brown@epita.fr>
2023-04-05 06:59:13 +0200

    feat: Run external program (WIP)
    - Create a first runner class that runs a external binary with arguments and returns if a segfault occured

--- tests/binaries/src/test_buffer_overflow.c
+++ tests/binaries/src/test_buffer_overflow.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char** argv) {
     volatile int modified;
     char buffer[24];
-    gets(buffer);
+
+    if(argc > 1) {
+        strcpy(buffer, argv[1]);
+    } else {
+        gets(buffer);
+    }
     if(modified != 0) {
         printf("Overflown.");
     } else {
         printf("Failed.");
     }
+    return 0;
 }
