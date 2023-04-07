5dcbe9442ff931eb4e187f30809e988440629816
mabocode <matthias.brown@epita.fr>
2023-04-07 19:37:34 +0200

    feat: Run external program (WIP)
    - Create the fuzzer class
    - Turn src folder into module
    - Add docstring for documentation

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+from .fuzzer import Fuzzer
+from .runner import Runner
+
+
+def main():
+    runner = Runner("./tests/binaries/bin/test_buffer_overflow")
+    fuzzer = Fuzzer()
+    print(fuzzer.run(runner))
+
+
+if __name__ == "__main__":
+    main()
