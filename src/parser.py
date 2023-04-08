633d5f9b9452b3bd63159f469593d84a640d3f67
me2te <me2te@kali.banadoura>
2023-04-08 17:17:30 +0200

    adding the first yaml code

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,7 @@
 import argparse
+import yaml
+
+#TODO : Add this code to principle class - check discord
 
 # Create an ArgumentParser object to parse the fuzzer options
 parser = argparse.ArgumentParser(description='Run the sfuzz fuzzer tool with options.')
@@ -14,16 +17,25 @@ parser.add_argument('--config', type=str, help='configuration file in YAML forma
 # Parse the command line arguments to extract the fuzzer options
 args = parser.parse_args()
 
-# Access the fuzzer options as attributes of the 'args' object
-num_jobs = args.jobs
-verbose = args.verbose
-char_list = args.chars
-output_file = args.output
-config_file = args.config
+
+# Parse the YAML configuration file
+if args.config:
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+        
+    # Extract the fuzzer options from the YAML configuration file
+    num_jobs = config.get('jobs', args.jobs)
+    verbose = config.get('verbose', args.verbose)
+    char_list = config.get('chars', args.chars)
+    output_file = config.get('output', args.output)
+else:
+    num_jobs = args.jobs
+    verbose = args.verbose
+    char_list = args.chars
+    output_file = args.output
 
 # Print the fuzzer options for verification
 print(f'Number of parallel jobs: {num_jobs}')
 print(f'Verbose output enabled: {verbose}')
 print(f'Character list: {char_list}')
 print(f'Output file: {output_file}')
-print(f'Config file: {config_file}')
